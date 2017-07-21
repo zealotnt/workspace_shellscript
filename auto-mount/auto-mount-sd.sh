0ada4e80d50f9d9d61087732dc7c1936b2019ad5
Tran Tam <tran.minh.tam@styl.solutions>
2017-07-21 09:08:22 +0700

    improve automount - use variables

--- auto-mount/auto-mount-sd.sh
+++ auto-mount/auto-mount-sd.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 SCRIPT_NAME=`basename "$0"`
-HDR="[$SCRIPT_NAME]"
+HDR="[${SCRIPT_NAME}]"
+SD_DEV="mmcblk1"
+SD_PART_PREFIX="${SD_DEV}p"
+SD_MOUNT_PREFIX="sd-$SD_PART_PREFIX"
 
-if [ ! -e /dev/mmcblk1 ]; then
+if [ ! -e /dev/SD_DEV ]; then
 	echo "$HDR No sdcard, suppose to do nothing"
 	exit 0
 fi
@@ -11,9 +14,9 @@ fi
 # We found sd card, mount all of them like the udev rule
 for SD_DEV_NUM in {1..9}
 do
-	if [ -e /dev/mmcblk1p$SD_DEV_NUM ]; then
-		echo "$HDR Detect mmcblk1p$SD_DEV_NUM, mount to /media/sd-mmcblk1p$SD_DEV_NUM"
-		mkdir -p /media/sd-mmcblk1p$SD_DEV_NUM
-		mount -o relatime /dev/mmcblk1p$SD_DEV_NUM /media/sd-mmcblk1p$SD_DEV_NUM
+	if [ -e /dev/$SD_PART_PREFIX$SD_DEV_NUM ]; then
+		echo "$HDR Detect $SD_PART_PREFIX$SD_DEV_NUM, mount to /media/$SD_MOUNT_PREFIX$SD_DEV_NUM"
+		mkdir -p /media/$SD_MOUNT_PREFIX$SD_DEV_NUM
+		mount -o relatime /dev/$SD_PART_PREFIX$SD_DEV_NUM /media/$SD_MOUNT_PREFIX$SD_DEV_NUM
 	fi
 done
