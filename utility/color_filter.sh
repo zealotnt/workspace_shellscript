5617a3e91614b0bdcb8f85c6a06d20f0783f9232
Tran Tam <tranminhtam.10192@gmail.com>
2018-04-06 15:18:11 +0700

    add docs for color_filter.sh

--- utility/color_filter.sh
+++ utility/color_filter.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# This script will remove all of the ANSI-Color code printed by the application
+# Usage:
+# ./color_filter.sh <some-app> <app's arguments>
+
+# References Resources:
+# https://eklitzke.org/stdout-buffering
+# https://unix.stackexchange.com/questions/25372/turn-off-buffering-in-pipe
+# http://www.pixelbeat.org/programming/stdio_buffering/
+# https://www.perkin.org.uk/posts/how-to-fix-stdio-buffering.html
+# http://www.tldp.org/LDP/abs/html/io-redirection.html
+
 # FILTER_REMOVE_ALL="s,Message,,g"
 # FILTER_REMOVE_ALL="s,1,1,g"
 # FILTER_REMOVE_ALL="s,\x1B\[[0-9;]*[a-zA-Z],,g"
